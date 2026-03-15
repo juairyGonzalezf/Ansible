84de1d2241cae3e8f65bab365efb102448ee6cb0
server6 <juairy.gonzalezf@outlook.com>
2026-03-15 20:00:53 +0100

    test

---
+++ sync_git.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+cd /etc/ansible/
+
+# Intentar sincronizar primero para evitar el error que te acaba de dar
+git pull server6 /etc/ansible --rebase
+
+# Añadir cambios nuevos
+git add .
+
+# Hacer commit solo si hay cambios (para no llenar el log de basura)
+if ! git diff-index --quiet HEAD --; then
+    git commit -m "Auto-sync: $(date +'%Y-%m-%d %H:%M:%S')"
+    git push server6 /etc/ansible
+fi
+
