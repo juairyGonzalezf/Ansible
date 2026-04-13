494b49102c590a6d79e0afc0c22341af043189ff
server6 <juairy.gonzalezf@outlook.com>
2026-04-13 11:19:04 +0200

    Auto-sync: 2026-04-13 11:19:04

--- sync_git.sh
+++ sync_git.sh
@@ -1,25 +1,16 @@
 #!/bin/bash
 
-# 1. Ir al directorio (si falla, aborta el script)
-cd /etc/ansible/ || exit 1
+#Ir al directorio (si falla, aborta el script)
+cd /etc/ansible/
 
-# 2. Añadir todos los cambios actuales al "Stage"
+# Intentar sincronizar primero para evitar el error que te acaba de dar
+#git pull server21 main
+
+# Añadir cambios nuevos
 git add .
 
-# 3. Comprobar si hay cambios y hacer commit ANTES del pull
-# Si el comando diff-index falla (es decir, hay cambios), hacemos commit.
+# Hacer commit solo si hay cambios (para no llenar el log de basura)
 if ! git diff-index --quiet HEAD --; then
     git commit -m "Auto-sync: $(date +'%Y-%m-%d %H:%M:%S')"
-    CAMBIOS_LOCALES=true
-else
-    CAMBIOS_LOCALES=false
-fi
-
-# 4. Ahora que el directorio está "limpio", sincronizamos (Pull + Rebase)
-# Bajamos los cambios del remoto y aplicamos nuestros commits locales por encima
-git pull  Ansible etc/ansible --rebase
-
-# 5. Si hubo cambios locales, los subimos al servidor
-if [ "$CAMBIOS_LOCALES" = true ]; then
-    git push  Ansible etc/ansible
+    git push Ansible etc/ansible
 fi
